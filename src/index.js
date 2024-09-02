da6818611f47293df0714fddc4fc3899cb0f5948
asrithasri <asrithk133@gmail.com>
2024-09-02 13:02:55 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import SpellCheckApp from "./SpellCheckApp";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <SpellCheckApp />
+  </StrictMode>
+);
